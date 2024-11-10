c046395516b1576a3e4cd9c9310e34da58aca294
Taha Khan <tahakhalid317@gmail.com>
2024-11-10 20:10:49 +0500

    Initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
+import { socialMedia } from "@/data";
+
+const Footer = () => {
+  return (
+    <footer className="w-full mb-[100px] md:mb-5 pb-10" id="contact">
+      <div className="flex flex-col items-center">
+        <h1 className="heading text-white lg:max-w-[45vw]">
+          Elevate <span className="text-purple">your</span> digital presence to
+          new heights!
+        </h1>
+        <p className="text-white-200 md:pt-10 my-5 text-center">
+          Connect with me today, and lets explore how I can help you achieve
+          your vision and reach your goals.
+        </p>
+
+        <a href="mailto:tahakhalid317@gmail.com">
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light text-white-200">
+          Copyright © 2024 Taha
+        </p>
+
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((info) => (
+            <div
+              key={info.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={info.img} alt="icons" width={20} height={20} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
